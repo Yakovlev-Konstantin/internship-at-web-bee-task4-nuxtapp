9d970af2767e63fb5eb4fc58820e6f11ff27fdb9
Konstantin Yakovlev <Yakovlev_Konstantin@mail.com>
2021-09-09 10:54:13 +0300

    Default Nuxt.js app files (almost)

---
+++ pages/time.vue
@@ -0,0 +1,47 @@
+<template>
+  <div><script>
+	window.onload = function () {
+	  sessionStorage.setItem("page", "time.html");
+	  document.location.href = ".";
+	};
+</script>
+
+<div class="column-left m-4">
+      <!-- card "Conservation with James" -->
+  	  <div class="accordion accordion-flush mb-4" id="accordionFlush1">
+  			<div class="accordion-item">
+    			<h2 class="accordion-header" id="flush-heading1">
+      		<button
+  	    			class="accordion-button collapsed"
+  	      			type="button"
+  	      			data-mdb-toggle="collapse"
+  	      			data-mdb-target="#flush-collapse1"
+  	      			aria-expanded="false"
+  	      			aria-controls="flush-collapse1"
+  	    		>	
+						<div class="d-flex justify-content-between align-items-center w-100">
+							<span class="card-text">Timer</span>
+  		  				</div>
+  	    		</button>
+  	  			</h2>
+  	  			<div
+  	    			id="flush-collapse1"
+  	    			class="accordion-collapse collapse position-relative"
+  	    			aria-labelledby="flush-heading1"
+  	    			data-mdb-parent="#accordionFlush1"
+  	  			>
+  	    			<div class="accordion-body m-3">
+						<p id="clock1" class="mb-0"></p>
+					</div>
+					
+  	  			</div>
+  				</div>
+			</div>
+		</div>
+	</div>
+</div></div>
+</template>
+
+<script>
+export default {}
+</script>
